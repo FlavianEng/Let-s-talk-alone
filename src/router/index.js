ef58becd76f1889c5f8ddfb862a1a2960c7877dc
flavian.eng <engevinflavian@gmail.com>
2021-09-17 19:04:31 +0200

    init + first step

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-];
+// const MainVue = () => import("");
+// const LostInTheVoid = () => import("");
+const Hello = () => import("../views/Hello.vue");
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+  history: createWebHistory(),
+  routes: [
+    { path: "/", redirect: "/home" },
+    { path: "/hello", component: Hello, props: true },
+    // { path: "*", component: LostInTheVoid },
+  ],
 });
 
 export default router;
--- src/views/Hello.vue
+++ src/views/Hello.vue
@@ -9,7 +9,7 @@
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: "Hello",
   props: {
     message: String,
   },
